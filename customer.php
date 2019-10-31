942ed444895aea84c9089386e98964882265fa1a
Paul Turpin <17347@csmrouen.net>
2019-10-31 16:14:10 +0100

    debut page customer

---
+++ customer.php
@@ -0,0 +1,61 @@
+<?php
+require ("include/header.php");
+?>
+<div class="container">
+    <div class="row">
+        <div class=".col-lg-4 col-md-4">
+            <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg" class="rounded-circle z-depth-0"
+                 alt="avatar image" style="height: 200px; width: 200px;">
+
+        </div>
+        <div class=".col-lg-4 col-md-4">
+            <div class="row">
+                <!-- Material unchecked -->
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="materialUnchecked">
+                    <label class="form-check-label" for="materialUnchecked">Material unchecked</label>
+                </div>
+                <p>Mail envoyé</p>
+            </div>
+            <div class="row">
+                <!-- Material unchecked -->
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="materialUnchecked">
+                    <label class="form-check-label" for="materialUnchecked">Material unchecked</label>
+                </div>
+                <p>SMS envoyé</p>
+            </div>
+            <div class="row">
+                <!-- Material unchecked -->
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="materialUnchecked">
+                    <label class="form-check-label" for="materialUnchecked">Material unchecked</label>
+                </div>
+                <p>Contacté via téléphone</p>
+            </div>
+
+        </div>
+        <div class=".col-lg-4 col-md-4">
+            <a href="#"><i class="fas fa-sms"></i></a>
+            <a href="#"><i class="fas fa-envelope-square"></i></a>
+            <a href="#"><i class="fas fa-phone"></i></a>
+
+        </div>
+        <i class="fas fa-phone-square-alt"></i>
+    </div>
+
+    <div class="row">
+        <div class=".col-lg-4 col-md-4">
+            <div class="card">
+                <p>Lorem</p>
+            </div>
+        </div>
+        <div class=".col-lg-8 col-md-8">
+            <div class="card">
+                <p>Lorem</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+require ('include/footer.php');
